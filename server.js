c0d0ef7c120b8b4cf15754c85d4b883a1991be70
SOUNDEPANEL <violetmjet@gmail.com>
2025-10-29 10:22:09 +0700

    Refactor static file serving and route handling

--- server.js
+++ server.js
@@ -4,18 +4,13 @@ const helmet = require('helmet');
 
 const app = express();
 
-// Security headers
 app.disable('x-powered-by');
 app.use(helmet());
 
-// Folder public
 const publicPath = path.join(__dirname, 'public');
-
-// Sajikan file statis
 app.use(express.static(publicPath));
 
-// Semua route SPA diarahkan ke index.html
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
